3169172f7683bd7874c68698afda1562cab30a52
Grasiela Gomes <produtoradama@gmail.com>
2021-06-21 09:57:08 -0300

    Environment variables

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,13 +4,13 @@ import "firebase/auth";
 import "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAQlFGVJLrWTDFCswxH1pjUfZSypkkqsP4",
-  authDomain: "letmeask-a19aa.firebaseapp.com",
-  databaseURL: "https://letmeask-a19aa-default-rtdb.firebaseio.com",
-  projectId: "letmeask-a19aa",
-  storageBucket: "letmeask-a19aa.appspot.com",
-  messagingSenderId: "341239312344",
-  appId: "1:341239312344:web:56c5a8db5846669411ccad"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_IO,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
