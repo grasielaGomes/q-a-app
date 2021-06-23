823e2f06069db7fe2a0dd62fe4c80731166a4853
Grasiela Gomes <produtoradama@gmail.com>
2021-06-23 09:59:21 -0300

    Room code copy

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,14 +1,23 @@
+import { useParams } from 'react-router';
 import logoImg from '../assets/images/logo.svg';
 import { Button } from '../componets/Button';
+import { RoomCode } from '../componets/RoomCode';
 import '../styles/room.scss'
 
+type RoomParams = {
+  id: string;
+}
+
 export function Room(){
+
+  const params = useParams<RoomParams>();
+
   return(
     <div id="page-room">
       <header>
         <div className="content">
           <img src={logoImg} alt="Letmeask" />
-          <div>codigo</div>
+          <RoomCode code={params.id}/>
         </div>
       </header>
       <main>
